0298fb4ad37deb07c3d33b87e5db2048bfc7a4e1
unknown <alessandro.dilello@.reedonline.local>
2011-01-18 16:44:28 +0000

    Added support for WCF 4 Web Http Services

---
+++ src/SharpArch/SharpArch.Wcf/NHibernate/WebServiceHost.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using System;
+
+namespace SharpArch.Wcf.NHibernate
+{
+    public class WebServiceHost : System.ServiceModel.Web.WebServiceHost
+    {
+        public WebServiceHost(Type serviceType, params Uri[] baseAddresses) : base(serviceType, baseAddresses) { }
+
+        protected override void OnOpening()
+        {
+            Description.Behaviors.Add(new ServiceBehavior());
+            base.OnOpening();
+        }
+    }
+}
---
+++ src/SharpArch/SharpArch.Wcf/NHibernate/WebServiceHostFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpArch.Wcf.NHibernate
+{
+    public class WebServiceHostFactory : System.ServiceModel.Activation.WebServiceHostFactory
+    {
+        protected override System.ServiceModel.ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses){
+            return new WebServiceHost(serviceType, baseAddresses);
+        }
+    }
+}
