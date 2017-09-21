fa2b3d5f23eff0a9dffe6d37a1275dbfec2185f6
vk <vk@alphacloud.net>
2017-09-20 22:59:00 -0400

    NetStandard migration, see #160

---
+++ Solutions/SharpArch.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SharpArch.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SharpArch.Domain")]
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("SharpArch.Tests")]
