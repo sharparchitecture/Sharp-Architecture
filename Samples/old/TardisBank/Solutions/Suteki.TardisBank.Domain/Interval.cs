14601006fb941f594f5a1865a1d6b29c4579d76f
vk <vk@alphacloud.net>
2018-07-21 14:26:49 -0400

    Cleanup

--- Samples/old/TardisBank/Solutions/Suteki.TardisBank.Domain/Interval.cs
+++
@@ -1,9 +0,0 @@
-namespace Suteki.TardisBank.Domain
-{
-    public enum Interval
-    {
-        Day = 1,
-        Week = 2,
-        Month = 3
-    }
-}
--- Samples/old/TardisBank/Solutions/Suteki.TardisBank.Infrastructure/x.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-#$x = @(Select-String -Path packages.config -Pattern id\s*=\s*"(.*?)"\s -AllMatches | %{$_.Matches} | %{" add package " + $_.Groups[1].Value})
-
-$x = @("--info", "add package )
-
-&"dotnet.exe" $x
