6c35cfaa76e01cd12416b3d053cd0b466c017344
vk <vk@alphacloud.net>
2018-09-30 22:23:00 -0400

    Cache action attribute

--- Samples/SharpArch.WebApi/Filters/HandleTransactionFilter.cs
+++ Samples/SharpArch.WebApi/Filters/HandleTransactionFilter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Serilog;
 using SharpArch.AspNetCore;
@@ -9,15 +11,35 @@ namespace SharpArch.WebApi.Filters
     {
         private static readonly ILogger Log = Serilog.Log.ForContext<HandleTransactionFilter>();
 
+        private static ImmutableDictionary<string, TransactionAttribute> _attributeCache
+            = ImmutableDictionary<string, TransactionAttribute>.Empty;
+        private SpinLock _lock = new SpinLock(false);
+
         public HandleTransactionFilter()
         {
-            Log.Debug("Created "+ GetHashCode().ToString("X"));
+            Log.Debug("Created handler "+ GetHashCode().ToString("X"));
         }
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var policy = context.FindEffectivePolicy<TransactionAttribute>();
-            Log.Information("Policy {@policy}", policy);
+            var actionId = context.ActionDescriptor.Id;
+            if (!_attributeCache.TryGetValue(actionId, out var transactionAttribute))
+            {
+                bool lockTaken = false;
+                _lock.Enter(ref lockTaken);
+                try
+                {
+                    transactionAttribute = context.FindEffectivePolicy<TransactionAttribute>();
+                    if (!_attributeCache.ContainsKey(actionId))
+                        _attributeCache = _attributeCache.Add(actionId, transactionAttribute);
+                }
+                finally
+                {
+                    if (lockTaken)
+                        _lock.Exit();
+                }
+            }
+            Log.Information("Policy {@policy}", transactionAttribute);
             base.OnActionExecuting(context);
         }
 
