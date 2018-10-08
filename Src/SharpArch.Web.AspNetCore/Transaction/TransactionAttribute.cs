2fb660026e411396502de0a43d2907214b9c2400
vk <vk@alphacloud.net>
2018-10-07 22:07:46 -0400

    Implement TransactionAttribute for ASP.NET Core, closes #175
    * Cache action attribute;
    * Update Tardis Bank samples

--- Src/SharpArch.Web.AspNetCore/Transaction/TransactionAttribute.cs
+++ Src/SharpArch.Web.AspNetCore/Transaction/TransactionAttribute.cs
@@ -1,20 +1,23 @@
-namespace SharpArch.AspNetCore
+using System;
+using System.Data;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SharpArch.AspNetCore.Transaction
 {
-    using System;
-    using System.Data;
-    using System.Runtime.CompilerServices;
-    using JetBrains.Annotations;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.Filters;
-    using SharpArch.Domain.PersistenceSupport;
-
     //todo: rewrite
-
     /// <summary>
-    ///     An attribute that implies a transaction per MVC action.
+    ///     An attribute that used to indicate that action must be wrapped in transaction.
     ///     <para>
-    ///         Transaction is either committed or rolled back after action execution is completed. See
-    ///         <see cref="OnActionExecuted" />.
+    ///         Attribute can be applied globally, at controller or at action level.
+    ///     </para>
+    ///     <para>
+    ///         Note: This is marker attribute only, <see cref="UnitOfWorkHandler" /> must be added to filter s
+    ///         collection in order to enable auto-transactions.
+    ///     </para>
+    ///     <para>
+    ///         Transaction is either committed or rolled back after action execution is completed.
     ///         Note: accessing database from the View is considered as a bad practice.
     ///     </para>
     /// </summary>
@@ -24,6 +27,57 @@ namespace SharpArch.AspNetCore
     ///     Transaction will be rolled back if there was unhandled exception in action or model validation was failed and
     ///     <see cref="RollbackOnModelValidationError" /> is <c>true</c>.
     /// </remarks>
+    [BaseTypeRequired(typeof(ControllerBase))]
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class TransactionAttribute : Attribute, IFilterMetadata
+    {
+        /// <summary>
+        ///     Gets or sets a value indicating whether rollback transaction in case of model validation error.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if transaction must be rolled back in case of model validation error; otherwise, <c>false</c>.
+        ///     Defaults to <c>true</c>.
+        /// </value>
+        public bool RollbackOnModelValidationError { get; }
+
+        /// <summary>
+        ///     Transaction isolation level.
+        /// </summary>
+        /// <value>Defaults to <c>ReadCommitted</c>.</value>
+        public IsolationLevel IsolationLevel { get; }
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="isolationLevel">Transaction isolation level.</param>
+        /// <param name="rollbackOnModelValidationError">
+        ///     indicates that transaction should be rolled back in case of
+        ///     model validation error.
+        /// </param>
+        public TransactionAttribute(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted, bool rollbackOnModelValidationError = true)
+        {
+            IsolationLevel = isolationLevel;
+            RollbackOnModelValidationError = rollbackOnModelValidationError;
+        }
+    }
+
+
+#if false
+/// <summary>
+///     An attribute that implies a transaction per MVC action.
+///     <para>
+///         Transaction is either committed or rolled back after action execution is completed. See
+///         <see cref="OnActionExecuted" />.
+///         Note: accessing database from the View is considered as a bad practice.
+///     </para>
+/// </summary>
+/// <remarks>
+///     Transaction will be committed after action execution is completed and no unhandled exception occurred, see
+///     <see cref="ActionExecutedContext.ExceptionHandled" />.
+///     Transaction will be rolled back if there was unhandled exception in action or model validation was failed and
+///     <see cref="RollbackOnModelValidationError" /> is <c>true</c>.
+/// </remarks>
     [PublicAPI]
     [BaseTypeRequired(typeof(ControllerBase))]
     public sealed class TransactionAttribute : ActionFilterAttribute
@@ -69,6 +123,7 @@ namespace SharpArch.AspNetCore
         /// <param name="filterContext"></param>
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            
             throw new NotImplementedException("Needs rewrite");
             if (TransactionManager == null)
                 throw new InvalidOperationException(
@@ -89,4 +144,5 @@ namespace SharpArch.AspNetCore
             return RollbackOnModelValidationError && filterContext.ModelState.IsValid == false;
         }
     }
+#endif
 }
