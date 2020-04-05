8dc27d171dd263b288a1cb0cf55361273e7d459f
vk <vk@alphacloud.net>
2020-04-05 16:48:39 -0400

    Update sample application

--- Samples/SharpArch.WebApi/App/Startup.cs
+++ Samples/SharpArch.WebApi/App/Startup.cs
@@ -1,14 +1,16 @@
 ﻿namespace SharpArch.WebApi.Sample
 {
     using System.Data;
-    using System.Globalization;
     using Autofac;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Http;
+#if NETCOREAPP2_1 || NETCOREAPP2_2
+    using System.Globalization;
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+#endif
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Newtonsoft.Json;
     using Stubs;
     using Web.AspNetCore.Transaction;
 
@@ -25,6 +27,15 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+#if NETCOREAPP3_1
+            services.AddControllers(options =>
+                {
+                    options.Filters.Add(new AutoTransactionHandler());
+                    options.Filters.Add(new TransactionAttribute(isolationLevel: IsolationLevel.Chaos));
+                })
+                .AddNewtonsoftJson();
+
+#else
             // Add framework services.
             services.AddMvcCore(options =>
                 {
@@ -39,7 +50,13 @@
                 })
                 .AddFormatterMappings()
                 .AddJsonFormatters()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+#if NETCOREAPP2_1
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+#elif NETCOREAPP2_2
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+#endif
+                ;
+#endif
 
             services.AddMemoryCache();
         }
@@ -47,7 +64,17 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+#if NETCOREAPP3_1
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+#else
+            app.UseAuthentication();
             app.UseMvc();
+#endif
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
