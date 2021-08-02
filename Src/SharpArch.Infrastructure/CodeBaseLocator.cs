dd6e18b81351d8c8903d179f61980ad87592ec72
vk <vk@alphacloud.net>
2021-08-01 23:12:02 -0400

    WIP: .NET 6 preview

--- Src/SharpArch.Infrastructure/CodeBaseLocator.cs
+++ Src/SharpArch.Infrastructure/CodeBaseLocator.cs
@@ -21,7 +21,7 @@
         public static string GetAssemblyCodeBasePath(Assembly assembly)
         {
             if (assembly == null) throw new ArgumentNullException(nameof(assembly));
-#if NET5_0
+#if NET5_0 || NET6_0
             var uri = new UriBuilder(assembly.Location);
 #else
             var uri = new UriBuilder(assembly.CodeBase);
